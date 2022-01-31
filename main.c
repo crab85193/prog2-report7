9ab29dc0f6da22eff6fb4942eb07fd8a85bc0045
crab85193 <crab85193@gmail.com>
2022-01-31 13:13:28 +0900

    [Update] update print_array method

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 void sort(int scores[]);
-void print_array(int scores[]);
+void print_array(char message[],int scores[]);
 
 int main(){
     const int SIZE = 5; //定数として配列のサイズを宣言
     int scores[SIZE] = {0, 60, 70, 100, 90};
     //処理コード
-    print_array(scores);
+    print_array("scores = ",scores);
     sort(scores);
-    print_array(scores);
+    print_array("results = ",scores);
     return 0;
 }
 
@@ -26,8 +26,10 @@ void sort(int scores[]){
     }
 }
 
-void print_array(int scores[]){
+void print_array(char message[],int scores[]){
+    printf("%s",message);
     for(int i=0; i<5; i++){
         printf("%d ",scores[i]);
     }
+    printf("\n");
 }
