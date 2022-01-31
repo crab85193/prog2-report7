eea0fda5a745870495837b0e7f5183baf5eabc3f
crab85193 <crab85193@gmail.com>
2022-01-31 13:42:42 +0900

    [Update] Getting the size of an element

--- main.c
+++ main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-void sort(int scores[]);
-void print_array(char message[],int scores[]);
+void sort(int scores[], int size);
+void print_array(char message[], int scores[], int size);
 
 int main(){
     const int SIZE = 5; //定数として配列のサイズを宣言
     int scores[SIZE] = {0, 60, 70, 100, 90};
     //処理コード
-    print_array("scores = ",scores);
-    sort(scores);
-    print_array("results = ",scores);
+    print_array("scores = ", scores, SIZE);
+    sort(scores, SIZE);
+    print_array("results = ", scores, SIZE);
     return 0;
 }
 
-void sort(int scores[]){
+void sort(int scores[], int size){
     int min;
-    for(int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size; j++){
             if(scores[i] > scores[j]){
                 min = scores[i];
 				scores[i] = scores[j];
@@ -26,10 +26,10 @@ void sort(int scores[]){
     }
 }
 
-void print_array(char message[],int scores[]){
-    printf("%s",message);
-    for(int i=0; i<5; i++){
-        printf("%d ",scores[i]);
+void print_array(char message[], int scores[], int size){
+    printf("%s", message);
+    for(int i=0; i<size; i++){
+        printf("%d ", scores[i]);
     }
     printf("\n");
 }
