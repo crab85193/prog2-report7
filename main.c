ef76de52093c603eaae419e01dbd12c87dc09937
crab85193 <crab85193@gmail.com>
2022-01-31 13:10:29 +0900

    [Update] add sort method

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+void sort(int scores[]);
 void print_array(int scores[]);
 
 int main(){
@@ -7,7 +8,22 @@ int main(){
     int scores[SIZE] = {0, 60, 70, 100, 90};
     //処理コード
     print_array(scores);
+    sort(scores);
+    print_array(scores);
     return 0;
+}
+
+void sort(int scores[]){
+    int min;
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            if(scores[i] > scores[j]){
+                min = scores[i];
+				scores[i] = scores[j];
+				scores[j] = min;
+            }
+        }
+    }
 }
 
 void print_array(int scores[]){
